291a5772147b1ab2773a4ff26b6e1180ecf5d75f
Anjil Bishowkarma <33276306+DeVAnJiL@users.noreply.github.com>
2019-02-25 22:58:06 +0545

    music for mainmenu added and some minor changes

--- Sky-Bomber/Game.cpp
+++ Sky-Bomber/Game.cpp
@@ -4,12 +4,32 @@
 #include <SFML/Graphics.hpp>
 #include <Windows.h>
 #include "../splashscreen.h"
+#include <SFML/Audio.hpp>
 
 
 void Game::start(){
 
-sf::RenderWindow window(sf::VideoMode(1366, 768), "Sky Bomber", sf::Style::Fullscreen);
+	sf::Music bgmusic;
+
+	if (!bgmusic.openFromFile("resources/mainmenu.wav"))
+		std::cout << "Cannot Load" << std::endl;
+
+	bgmusic.setVolume(20.0f);
+	sf::SoundBuffer enter, selecting;
+	sf::Sound entersound, selectingsound;
+
+	if (!enter.loadFromFile("resources/entering.wav"))
+		std::cout << "can't find sound" << std::endl;
+	entersound.setBuffer(enter);
+
+	if (!selecting.loadFromFile("resources/buttonpress.wav"))
+		std::cout << "can't find sound" << std::endl;
+	selectingsound.setBuffer(selecting);
+	
+
+sf::RenderWindow window(sf::VideoMode(1366, 768), "Sky Bomber");
 sf::Texture mainmenu;
+bgmusic.play();
 if (!mainmenu.loadFromFile("resources/splash.png"))
 {
 	std::cout << "Load failed" << std::endl;
@@ -29,14 +49,20 @@ while (window.isOpen())
 
 		switch (event.type)
 		{
+			
 		case sf::Event::KeyReleased:
 			switch (event.key.code)
 			{
 			case sf::Keyboard::Up:
+				selectingsound.play();
+			//	Manager::menuselectsound();
 				menu.MoveUp();
 				break;
 
 			case sf::Keyboard::Down:
+				//menubuttonsound.play();
+				selectingsound.play();
+				//Manager::enter();
 				menu.MoveDown();
 				break;
 
@@ -45,14 +71,18 @@ while (window.isOpen())
 				{
 				case 0:
 					window.close();
+					entersound.play();
+					bgmusic.pause();
 					Engine::start();
 
 					std::cout << "Play button has been pressed" << std::endl;
 					break;
 				case 1:
+					//enter.play();
 					std::cout << "HighScore should be displayed" << std::endl;
 					break;
 				case 2:
+					//enter.play();
 					std::cout << "Game Exit" << std::endl;
 					window.close();
 					break;
@@ -71,7 +101,7 @@ while (window.isOpen())
 	}
 
 	window.clear();
-
+	
 	window.draw(mainscreensprite);
 	menu.draw(window);
 
