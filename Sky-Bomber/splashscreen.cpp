e83bb00476227a9e088c75a5f06d619c6a1015e3
Anjil Bishowkarma <33276306+DeVAnJiL@users.noreply.github.com>
2019-02-25 22:38:18 +0545

    2 photo added for splash screen and +sound effects

--- Sky-Bomber/splashscreen.cpp
+++ Sky-Bomber/splashscreen.cpp
@@ -4,35 +4,59 @@
 #include <SFML/Graphics.hpp>
 #include <Windows.h>
 #include "splashscreen.h"
+#include <SFML/Audio.hpp>
 using namespace std;
 
 void Splash::start() {
+	
+	sf::Music bgmusic;
+	//sound for intro or splashscreen music
+	if (!bgmusic.openFromFile("resources/intro.wav"))
+		std::cout << "Cannot Load" << std::endl;
+
+
 	sf::RenderWindow window(sf::VideoMode(1366, 768), "Sky Bomber");
-	sf::Texture splash;
-	if (!splash.loadFromFile("resources/splashscreen.png"))
+	sf::Texture splash1, splash2;
+	bgmusic.play();
+	bgmusic.setVolume(10.f);
+	//first picture in splash
+	if (!splash1.loadFromFile("resources/splash1.png"))
 	{
 		std::cout << "Load Failed" << std::endl;
 	}
-	sf::Sprite splashsprite;
-	splashsprite.setTexture(splash);
+	//second picture in splash
+	if (!splash2.loadFromFile("resources/splash2.png"))
+	{
+		std::cout << "load failed" << std::endl;
+	}
+
+	//giving texture to sprites
+	sf::Sprite splash1sprite, splash2sprite;
+	splash1sprite.setTexture(splash1);
+	splash2sprite.setTexture(splash2);
 
 
-	// run the program as long as the window is open
 	while (window.isOpen())
 	{
-		// check all the window's events that were triggered since the last iteration of the loop
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			// "close requested" event: we close the window
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+		//for splash1
 		window.clear();
-
-		window.draw(splashsprite);
+		window.draw(splash1sprite);
 		window.display();
-		Sleep(5000);
+		Sleep(3000);
+		//for splash2
+		window.clear();
+		window.draw(splash2sprite);
+		window.display();
+		Sleep(3000);
+		bgmusic.pause();
 		window.close();
 	}
+	//after the splash terminate or works well the main screen fuction called
+	Game::start();
 }
