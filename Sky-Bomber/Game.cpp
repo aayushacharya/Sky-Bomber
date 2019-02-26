7b8faef4731c04dd32676a5e6f2935d0e64e7b9a
Anjil Bishowkarma <33276306+DeVAnJiL@users.noreply.github.com>
2019-02-26 18:49:27 +0545

    Minor Changes

--- Sky-Bomber/Game.cpp
+++ Sky-Bomber/Game.cpp
@@ -10,101 +10,92 @@
 void Game::start(){
 
 	sf::Music bgmusic;
-
-	if (!bgmusic.openFromFile("resources/mainmenu.wav"))
-		std::cout << "Cannot Load" << std::endl;
-
-	bgmusic.setVolume(20.0f);
 	sf::SoundBuffer enter, selecting;
 	sf::Sound entersound, selectingsound;
 
+	if (!bgmusic.openFromFile("resources/mainmenu.wav"))
+		std::cout << "Cannot Stream the Music" << std::endl;
 	if (!enter.loadFromFile("resources/entering.wav"))
-		std::cout << "can't find sound" << std::endl;
-	entersound.setBuffer(enter);
-
+		std::cout << "Cannot Open File" << std::endl;
 	if (!selecting.loadFromFile("resources/buttonpress.wav"))
 		std::cout << "can't find sound" << std::endl;
+
+	//Setting Buffer
+	entersound.setBuffer(enter);
 	selectingsound.setBuffer(selecting);
 	
+	//Rendering Window
+	sf::RenderWindow window(sf::VideoMode(1366, 768), "Sky Bomber");
+	bgmusic.play();			//Background Music
 
-sf::RenderWindow window(sf::VideoMode(1366, 768), "Sky Bomber");
-sf::Texture mainmenu;
-bgmusic.play();
-if (!mainmenu.loadFromFile("resources/splash.png"))
-{
-	std::cout << "Load failed" << std::endl;
-	//system("pause");
-}
-sf::Sprite mainscreensprite;
-mainscreensprite.setTexture(mainmenu);
+	sf::Texture mainmenu;
+	if (!mainmenu.loadFromFile("resources/splash.png"))		
+		std::cout << "Load failed" << std::endl;
 
-Menu menu(window.getSize().x, window.getSize().y);
+	sf::Sprite mainscreensprite;
+	mainscreensprite.setTexture(mainmenu);
 
-while (window.isOpen())
-{
-	sf::Event event;
+	//Menu Main Content
+	Menu menu(window.getSize().x, window.getSize().y);
 
-	while (window.pollEvent(event))
-	{
+	while (window.isOpen())
+	{	
+		sf::Event event;
 
-		switch (event.type)
-		{
-			
-		case sf::Event::KeyReleased:
-			switch (event.key.code)
+		while (window.pollEvent(event))
 			{
-			case sf::Keyboard::Up:
-				selectingsound.play();
-			//	Manager::menuselectsound();
-				menu.MoveUp();
-				break;
 
-			case sf::Keyboard::Down:
-				//menubuttonsound.play();
-				selectingsound.play();
-				//Manager::enter();
-				menu.MoveDown();
-				break;
+				switch (event.type)
+					{
+						case sf::Event::KeyReleased:
+						switch (event.key.code)
+							{
+								case sf::Keyboard::Up:
+								selectingsound.play();
+								menu.MoveUp();
+								break;
 
-			case sf::Keyboard::Return:
-				switch (menu.GetPressedItem())
-				{
-				case 0:
-					window.close();
-					entersound.play();
-					bgmusic.pause();
-					Engine::start();
+								case sf::Keyboard::Down:
+								selectingsound.play();
+								menu.MoveDown();
+								break;
 
-					std::cout << "Play button has been pressed" << std::endl;
-					break;
-				case 1:
-					//enter.play();
-					std::cout << "HighScore should be displayed" << std::endl;
-					break;
-				case 2:
-					//enter.play();
-					std::cout << "Game Exit" << std::endl;
-					window.close();
-					break;
-				}
+								case sf::Keyboard::Return:
+								switch (menu.GetPressedItem())
+									{
+										case 0:
+										window.close();
+										entersound.play();
+										bgmusic.pause();
+										Engine::start();
+										std::cout << "Play button has been pressed" << std::endl;
+										break;
 
-				break;
+										case 1:
+										std::cout << "HighScore should be displayed" << std::endl;
+										break;
+
+										case 2:
+										std::cout << "Game Exit" << std::endl;
+										window.close();
+										break;
+									}
+
+									break;
+							}
+
+						break;
+
+						case sf::Event::Closed:
+						window.close();
+
+						break;
+					}
 			}
 
-			break;
-		case sf::Event::Closed:
-			window.close();
-
-			break;
-
-		}
+		window.clear();
+		window.draw(mainscreensprite);
+		menu.draw(window);
+		window.display();
 	}
-
-	window.clear();
-	
-	window.draw(mainscreensprite);
-	menu.draw(window);
-
-	window.display();
-}
 }
