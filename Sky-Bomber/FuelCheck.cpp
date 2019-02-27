5987a49a340236c182dc5d79422a986a3b5043eb
QuizzerNirajan <33093414+QuizzerNirajan@users.noreply.github.com>
2019-02-27 23:37:50 +0545

    Fuel status resolved and visible on the window
    Fuel status visible

--- Sky-Bomber/FuelCheck.cpp
+++ Sky-Bomber/FuelCheck.cpp
@@ -2,38 +2,51 @@
 #include<SFML/Graphics.hpp>
 #include<SFML/System/Clock.hpp>
 #include<iostream>
-bool FuelCheck::isFinished(bool collision)
+bool FuelCheck::isFinished(bool collision )
 {
 	static sf::Clock clk;
+	static int previousTime= 0;
 	static int startFuel = 30;
 	static bool fuelFinished = false;
-	if (clk.getElapsedTime().asSeconds() > 10.0f)
+	static float dt = 0.0f;
+	dt += clk.getElapsedTime().asSeconds();
+	std::cout << int(dt) << std::endl;
+
+	if (int (dt) > 0 && int(dt)!=previousTime )
+
 	{
-		
-		
-			startFuel -= 10;
+		if (int(dt) % 10 == 0) {
+			startFuel -= 10; 
+			std::cout << "Fuel Decreases";
+		}
 		
 
-
-		if (startFuel == 0)
+        if (startFuel == 0)
 		{
 			fuelFinished = true;
 		}
-		return fuelFinished;
-		clk.restart();
+		
+
 	}
+	previousTime = int(dt);
+	
 	if (collision)
 	{
 		if (startFuel < 30)
 		{
+			std::cout << "Fuel Increases";
 			startFuel += 10;
 		}
 		else
 		{
 			startFuel = 30;
 		}
-		clk.restart();
+		
+		std::cout << "Mothersndsjhdjshdjjshdjshdjhsjhdjshdjhsd " << std::endl;
 
 	}
+	setFuel(startFuel);
+	std::cout << "The value is :" <<getFuel() << "\n";
+	clk.restart();
 	return fuelFinished;
 }
