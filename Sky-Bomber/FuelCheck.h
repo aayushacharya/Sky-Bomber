c4439a3860152fa34ebc4a117cbe510479655294
Anjil Bishowkarma <33276306+DeVAnJiL@users.noreply.github.com>
2019-02-28 00:19:10 +0545

    HUD update

--- Sky-Bomber/FuelCheck.h
+++ Sky-Bomber/FuelCheck.h
@@ -1,21 +1,30 @@
-
 #pragma once
 #include<SFML/Graphics.hpp>
 
 class FuelCheck {
 public:
-	  int currentFuel;
-	  FuelCheck()
-	  {
-		 
-	  }
+	int currentFuel;
+	int currentLife;
+
+	FuelCheck()
+	{
+
+	}
 	bool isFinished(bool collision = false);
-	void setFuel(int ccurrentFuel)
+	void setFuel(int ccurrentFuel )
 	{
 		currentFuel = ccurrentFuel;
 	}
 	int getFuel()
 	{
 		return currentFuel;
+	}
+	void setLife(int ccurrentLife)
+	{
+		currentLife = ccurrentLife;
+	}
+	int getLife()
+	{
+		return currentLife;
 	}
 };
