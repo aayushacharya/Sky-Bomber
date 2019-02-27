29b989b66b886bd7d66fab6d553fd422d94aca54
Anjil Bishowkarma <33276306+DeVAnJiL@users.noreply.github.com>
2019-02-28 00:19:46 +0545

    HUD updated

--- Sky-Bomber/HUD.cpp
+++ Sky-Bomber/HUD.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <sstream>
 using namespace std;
-LifeFuel::LifeFuel(float width, float height, int fuel)
+LifeFuel::LifeFuel(float width, float height, int fuel, int life)
 {
 
 	// << "The value is :" << fuel << "\n";
@@ -12,20 +12,21 @@ LifeFuel::LifeFuel(float width, float height, int fuel)
 		std::cout << "Loading Failed" << std::endl;
 	}
 
-	stringstream ss;
-	
-	ss.str("");
-	ss << fuel;
+	stringstream FuelStatus, Lifes;
+
+	FuelStatus.str("");
+	Lifes << life;
+	FuelStatus << fuel;
 
 	Lifefuel[0].setFont(font);
 	Lifefuel[0].setColor(sf::Color::Black);
-	Lifefuel[0].setString(ss.str());
+	Lifefuel[0].setString(Lifes.str());
 	Lifefuel[0].setScale(1.5, 1.5);
-	Lifefuel[0].setPosition(sf::Vector2f(width-1346.0f, height - 738.0f));
+	Lifefuel[0].setPosition(sf::Vector2f(width - 1346.0f, height - 738.0f));
 
 	Lifefuel[1].setFont(font);
 	Lifefuel[1].setColor(sf::Color::Black);
-	Lifefuel[1].setString("10");
+	Lifefuel[1].setString(FuelStatus.str());
 	Lifefuel[1].setScale(1.5, 1.5);
 	Lifefuel[1].setPosition(sf::Vector2f(width - 66.0f, height - 738.0f));
 
@@ -43,6 +44,3 @@ void LifeFuel::draw(sf::RenderWindow &window)
 		window.draw(Lifefuel[i]);
 	}
 }
-
-
-
